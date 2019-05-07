72289230b1e123b0f0dbde0d2fd6a4efb055f594
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-05-07 17:03:48 -0300

    Add files via upload

---
+++ modificadores.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+  int i = 10;
+  short u;
+  printf("%i - variável i\n", sizeof(i));
+  printf("%i - variável u\n", sizeof(u));
+  return 0;
+}
