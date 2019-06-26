5e71f00518ec9de6f24c52ecfc0ee1b49d457ac4
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-06-25 21:02:08 -0300

    Add files via upload

--- Aula8/exercicio1.c
+++ Aula8/exercicio1.c
@@ -17,40 +17,59 @@ struct Pessoa funcionario[2];
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
-	int i,j;
-	for(i=0; i<2; i++){
+	int i,j, op;
+	do{	
 		system("cls");
-		printf("Informe o Código: ");
-		fflush(stdin);
-		scanf("%d", &funcionario[i].codigo);
-		printf("Informe o nome:  ");
-		fflush(stdin);
-		gets(funcionario[i].nome);
-		printf("Informe o sobrenome:  ");
-		fflush(stdin);
-		gets(funcionario[i].sobrenome);
-		printf("Informe a data de Nascimento:  ");
-		fflush(stdin);
-		gets(funcionario[i].dataNascimento);
-		printf("Informe a data de Admissão:  ");
-		fflush(stdin);
-		gets(funcionario[i].dataAdmissao);
-		printf("Informe a data de o RG");
-		gets(funcionario[i].rg);
-		printf("Informe o Salário");
-		scanf("%f", &funcionario[i].salario);
-	}
-	printf("  ITEM - CODIGO - NOME - SOBRENOME - DATA NASCIMENTO - DATA ADMISSÃO - RG - SALÁRIO (R$)\n");
-	//int j;
-	for(j=0;j<2;j++){
-		printf("\n    %d   - %d  ", j, funcionario[j].codigo);
-		printf(" - %s", funcionario[j].nome);
-		printf(" - %s", funcionario[j].sobrenome);
-		printf("   - %s", funcionario[j].dataNascimento);
-		printf("   - %s", funcionario[j].dataAdmissao);
-		printf("   - %s", funcionario[j].rg);
-		printf("   - %f", funcionario[j].salario);
-	}
+		printf("\n\t1 - Incluir");
+		printf("\n\t2 - listar");
+		printf("\n\t3 - Sair");
+		printf("\nEscolha uma opção: ");
+		scanf("%d", &op);
+		
+		switch(op){
+			case 1:
+				for(i=0; i<2; i++){
+					system("cls");
+					printf("Informe o Código: ");
+					fflush(stdin);
+					scanf("%d", &funcionario[i].codigo);
+					printf("Informe o nome:  ");
+					fflush(stdin);
+					gets(funcionario[i].nome);
+					printf("Informe o sobrenome:  ");
+					fflush(stdin);
+					gets(funcionario[i].sobrenome);
+					printf("Informe a data de Nascimento:  ");
+					fflush(stdin);
+					gets(funcionario[i].dataNascimento);
+					printf("Informe a data de Admissão:  ");
+					fflush(stdin);
+					gets(funcionario[i].dataAdmissao);
+					printf("Informe a data de o RG");
+					gets(funcionario[i].rg);
+					printf("Informe o Salário");
+					scanf("%f", &funcionario[i].salario);
+				}		
+				break;
+			case 2:
+				printf("  ITEM - CODIGO - NOME - SOBRENOME - DATA NASCIMENTO - DATA ADMISSÃO - RG - SALÁRIO (R$)\n");
+				for(j=0;j<2;j++){
+					printf("\n    %d   - %d  ", j, funcionario[j].codigo);
+					printf(" - %s", funcionario[j].nome);
+					printf(" - %s", funcionario[j].sobrenome);
+					printf("   - %s", funcionario[j].dataNascimento);
+					printf("   - %s", funcionario[j].dataAdmissao);
+					printf("   - %s", funcionario[j].rg);
+					printf("   - %f", funcionario[j].salario);
+				}
+				system("pause");
+				break;
+			default:
+				printf("Saindo do sistema!!!");
+		}
+	}while(op != 3);
+		//int j;
+		
 	printf("\n\n");
 	system("pause");
 	return 0;
